02bf071861db9915b982b8afb1e263c6cd11953e
Chamith Shehan Weerarathna <66713045+shehanweerarathne@users.noreply.github.com>
2022-06-21 10:32:02 +0530

    H2 database update

--- car-sale/src/main/java/com/example/carsale/CarSaleApplication.java
+++ car-sale/src/main/java/com/example/carsale/CarSaleApplication.java
@@ -1,13 +1,24 @@
 package com.example.carsale;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.carsale.domain.Car;
+import com.example.carsale.domain.CarRepository;
 
 @SpringBootApplication
-public class CarSaleApplication {
+public class CarSaleApplication implements CommandLineRunner {
+	@Autowired
+	private CarRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarSaleApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
